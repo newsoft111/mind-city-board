9b6a4451cc477fb1193e99bcb77eb5cfd7d92182
newsoft111 <ydj0617@gmail.com>
2024-10-24 14:38:27 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,17 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import NextLink from 'next/link';
+'use client'
 
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
-  return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI - Next.js App Router example in TypeScript
-        </Typography>
-        <Link href="/about" color="secondary" component={NextLink}>
-          Go to the about page
-        </Link>
+  const router = useRouter();
 
-      </Box>
-    </Container>
+  useEffect(() => {
+    // 페이지가 로드될 때 /board/free로 리다이렉트
+    router.push('/board/free');
+  }, [router]);
+
+  return (
+    <></>
   );
 }
